c94021431f2e8ff65b7a4be6627603407d2939dc
Wishmastering <charsdb@gmail.com>
2023-09-22 10:26:33 -0600

    more interface and type testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  type Animal = "Dog" | "Cat" | "Fish" | "Bird";
+  type Domestic = "Dog" | "Cat" | "Parrot";
+
+  // Extract domestic animals from the list of animals
+  type DomesticAnimals = Extract<Animal, Domestic>; // 'Dog' || 'cat'
+
+  // Exclude domestic animals from the list of animals
+  type WildAnimals = Exclude<Animal, Domestic>; // "Fish" | "Bird"
+
+  const housePet: DomesticAnimals = "Cat";
+  const outdoorPet: WildAnimals = "Bird";
+
+  interface EmployeeType {
+    id: number;
+    fullname: string;
+    role: string;
+  }
+
+  const employees: Record<number, EmployeeType> = {
+    0: { id: 1, fullname: "John Doe", role: "Designer" },
+    1: { id: 2, fullname: "Ibrahima Fall", role: "Developer" },
+    2: { id: 3, fullname: "Sara Duckson", role: "Developer" },
+  };
+
+  // 0: { id: 1, fullname: "John Doe", role: "Designer" },
+  // 1: { id: 2, fullname: "Ibrahima Fall", role: "Developer" },
+  // 2: { id: 3, fullname: "Sara Duckson", role: "Developer" }
 
   return (
     <>
@@ -16,20 +44,29 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>
+        Vite + React + {housePet} & {outdoorPet}
+      </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section>
+        <ul>
+          {Object.values(employees).map((employee) => (
+            <li key={employee.id}>
+              <strong>ID:</strong> {employee.id}
+              <br />
+              <strong>Full Name:</strong> {employee.fullname}
+              <br />
+              <strong>Role:</strong> {employee.role}
+            </li>
+          ))}
+        </ul>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
